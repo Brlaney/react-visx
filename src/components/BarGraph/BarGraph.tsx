d0dad859a33327ba51b7c6aeed20b89160f086af
brlaney42 <brlaney@outlook.com>
2021-08-16 10:19:00 -0500

    Still debugging BarGraph

--- src/components/BarGraph/BarGraph.tsx
+++ src/components/BarGraph/BarGraph.tsx
@@ -1,8 +1,8 @@
-import { letterFrequency } from '@visx/mock-data';
+// import { letterFrequency } from '@visx/mock-data';
 import { Group } from '@visx/group';
 import { Bar } from '@visx/shape';
-import { scaleLinear, scaleBand } from '@visx/scale';
-import { data } from '../lib/data';
+// import { scaleLinear, scaleBand } from '@visx/scale';
+import { data } from '../../lib/data';
 
 
 // Define the graph dimensions and margins
@@ -14,42 +14,46 @@ const margin = { top: 20, bottom: 20, left: 20, right: 20 };
 const xMax = width - margin.left - margin.right;
 const yMax = height - margin.top - margin.bottom;
 
+const x = data[0];
+const y = data[1];
+
 // We'll make some helpers to get at the data we want
-const x = d => d.letter;
-const y = d => +d.frequency * 100;
+// const x = d => d.letter;
+// const y = d => +d.frequency * 100;
 
 // And then scale the graph by our data
-const xScale = scaleBand({
-  range: [0, xMax],
-  round: true,
-  domain: data.map(x),
-  padding: 0.4,
-});
-const yScale = scaleLinear({
-  range: [yMax, 0],
-  round: true,
-  domain: [0, Math.max(...data.map(y))],
-});
+// const xScale = scaleBand({
+//   range: [0, xMax],
+//   round: true,
+//   domain: data.map(x),
+//   padding: 0.4,
+// });
+// const yScale = scaleLinear({
+//   range: [yMax, 0],
+//   round: true,
+//   domain: [0, Math.max(...data.map(y))],
+// });
 
-// Compose together the scale and accessor functions to get point functions
-const compose = (scale, accessor) => data => scale(accessor(data));
-const xPoint = compose(xScale, x);
-const yPoint = compose(yScale, y);
+// // Compose together the scale and accessor functions to get point functions
+// const compose = (scale, accessor) => data => scale(accessor(data));
+// const xPoint = compose(xScale, x);
+// const yPoint = compose(yScale, y);
+
 
 // Finally we'll embed it all in an SVG
 const BarGraph = ({data}) => {
   return (
     <svg width={width} height={height}>
       {data.map((d: any, i: number) => {
-        const barHeight = yMax - yPoint(d);
+        const barHeight = yMax - 70;
         return (
           <Group key={`bar-${i}`}>
             <Bar
-              x={xPoint(d)}
-              y={yMax - barHeight}
+              x={x}
+              y={y}
               height={barHeight}
-              width={xScale.bandwidth()}
-              fill="#943E46"
+              width={400}
+              fill='#943E46'
             />
           </Group>
         );
@@ -59,3 +63,24 @@ const BarGraph = ({data}) => {
 }
 
 export default BarGraph;
+
+// const BarGraph = ({data}) => {
+//   return (
+//     <svg width={width} height={height}>
+//       {data.map((d: any, i: number) => {
+//         const barHeight = yMax - yPoint(d);
+//         return (
+//           <Group key={`bar-${i}`}>
+//             <Bar
+//               x={xPoint(d)}
+//               y={yMax - barHeight}
+//               height={barHeight}
+//               width={xScale.bandwidth()}
+//               fill="#943E46"
+//             />
+//           </Group>
+//         );
+//       })}
+//     </svg>
+//   );
+// }
